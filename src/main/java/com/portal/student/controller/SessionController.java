e23b49e421979ca064a5f0e818e71d9342f20a0c
Saswat <saswat23@gmail.com>
2019-12-03 11:05:53 +0530

    Code Refactor:
    Changed @RequestMapping with respective method mappings (i.e
    @GetMapping, @PostMapping, @PutMapping, @DeleteMapping)

--- src/main/java/com/portal/student/controller/SessionController.java
+++ src/main/java/com/portal/student/controller/SessionController.java
@@ -2,9 +2,10 @@ package com.portal.student.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.portal.student.entity.Student;
@@ -18,22 +19,20 @@ public class SessionController {
 
 	@Autowired
 	SessionService service;
-//	@Autowired
-//	Student student;
 
-	@RequestMapping(value = "/", method = RequestMethod.GET)
+	@GetMapping(value = "/")
 	public String home() {
 		return "Welcome to homepage";
 	}
 
-	@RequestMapping(value = "/login", method = RequestMethod.POST, consumes = "application/json")
+	@PostMapping(value = "/login", consumes = "application/json")
 	public ResponseEntity<LoginResponse> login(@RequestBody LoginRequest request) {
 		Student student = new Student(request.getUsername(), request.getPassword());
 		LoginResponse response = service.validateLogin(student);
 		return new ResponseEntity<LoginResponse>(response, response.getStatus());
 	}
 
-	@RequestMapping(value = "/logout", method = RequestMethod.POST)
+	@PostMapping(value = "/logout")
 	public String logout() {
 		return "";
 	}

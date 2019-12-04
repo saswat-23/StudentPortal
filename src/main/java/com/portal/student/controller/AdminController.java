e22102972c5eff7c37f9fc5a7a8b451611da0f19
Saswat <saswat23@gmail.com>
2019-12-04 08:30:32 +0530

    Added CORS support for all controllers.

--- src/main/java/com/portal/student/controller/AdminController.java
+++ src/main/java/com/portal/student/controller/AdminController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -22,6 +23,7 @@ import com.portal.student.model.LoginResponse;
 import com.portal.student.service.AdminService;
 import com.portal.student.service.SessionService;
 
+@CrossOrigin
 @RestController
 @RequestMapping(value = "/admin")
 public class AdminController {
--- src/main/java/com/portal/student/controller/SessionController.java
+++ src/main/java/com/portal/student/controller/SessionController.java
@@ -2,6 +2,7 @@ package com.portal.student.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,6 +14,7 @@ import com.portal.student.model.LoginRequest;
 import com.portal.student.model.LoginResponse;
 import com.portal.student.service.SessionService;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/session")
 public class SessionController {
